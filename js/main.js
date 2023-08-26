d9375c71fc03fcc93fe0c83cd6021a0d08522fb4
alamin <alaminhosen44000@gmail.com>
2023-08-26 16:43:50 +0600

    a

---
+++ js/main.js
@@ -0,0 +1,45 @@
+(function ($) {
+  "use strict";
+
+  // offcanvas-js
+  $(".offcanvas-open").click(function () {
+    $(".offcanvas-menu").addClass("active");
+    $(".offcanvas-overlay").addClass("active");
+  });
+  $(".offcanvas-menu a").click(function () {
+    $(".offcanvas-menu").removeClass("active");
+    $(".offcanvas-overlay").removeClass("active");
+  });
+  $(".close-offcanvas").click(function () {
+    $(".offcanvas-menu").removeClass("active");
+    $(".offcanvas-overlay").removeClass("active");
+  });
+  $(document).mouseup(function (e) {
+    var container = $(".offmenu");
+
+    // If the target of the click isn't the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      $(".offcanvas-menu").removeClass("active");
+      $(".offcanvas-overlay").removeClass("active");
+    }
+  });
+
+  // owl-carousel
+  $(".hear-slider-active").owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    responsiveClass: true,
+    nav: false,
+    dots: true,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+  });
+})(jQuery);
+
+$(document).ready(function () {
+  // preloader
+  $("#preloader").fadeOut(500);
+});
